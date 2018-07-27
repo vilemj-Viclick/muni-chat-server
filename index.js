3b46211ec9a8d693166a640151a3186b8e8efb4f
Vilém Jeniš <vilemj@kentico.com>
2018-07-27 12:04:16 +0200

    Make server work.

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const uuid = require('uuid/v4');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+let messages = [];
+
+function isString(arg) {
+  return typeof arg === 'string';
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/messages', (req, res) => res.send(JSON.stringify(messages, null, 4)))
+  .post('/message', (req, res) => {
+    try {
+      console.log(req.body);
+      const message = req.body;
+      if (isString(message.text) && isString(message.nick)) {
+        console.log(message);
+        messages.push({
+          ...message,
+          id: uuid(),
+        });
+        while (messages.length > 50) {
+          messages.shift();
+        }
+        res.end();
+      }
+      else {
+        res.writeHead(400, 'The message does not contain a string text and a string nick.');
+        res.end();
+      }
+    }
+    catch {
+      res.writeHead(400, 'Body is not a valid JSON.');
+      res.end();
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
