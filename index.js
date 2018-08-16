2d3fba6b51c8f0ab25dfc593dd15de07127d0295
Vilém Jeniš <vilemj@kentico.com>
2018-08-16 12:34:35 +0200

    Try logging an error.

--- index.js
+++ index.js
@@ -10,36 +10,41 @@ function isString(arg) {
   return typeof arg === 'string';
 }
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/messages', (req, res) => res.send(JSON.stringify(messages, null, 4)))
-  .post('/message', (req, res) => {
-    try {
-      console.log(req.body);
-      const message = req.body;
-      if (isString(message.text) && isString(message.nick)) {
-        console.log(message);
-        messages.push({
-          ...message,
-          id: uuid(),
-        });
-        while (messages.length > 50) {
-          messages.shift();
+try {
+  express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json())
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/messages', (req, res) => res.send(JSON.stringify(messages, null, 4)))
+    .post('/message', (req, res) => {
+      try {
+        console.log(req.body);
+        const message = req.body;
+        if (isString(message.text) && isString(message.nick)) {
+          console.log(message);
+          messages.push({
+            ...message,
+            id: uuid(),
+          });
+          while (messages.length > 50) {
+            messages.shift();
+          }
+          res.end();
         }
+        else {
+          res.writeHead(400, 'The message does not contain a string text and a string nick.');
+          res.end();
+        }
+      }
+      catch {
+        res.writeHead(400, 'Body is not a valid JSON.');
         res.end();
       }
-      else {
-        res.writeHead(400, 'The message does not contain a string text and a string nick.');
-        res.end();
-      }
-    }
-    catch {
-      res.writeHead(400, 'Body is not a valid JSON.');
-      res.end();
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+}
+catch (error) {
+  console.log(error);
+}
